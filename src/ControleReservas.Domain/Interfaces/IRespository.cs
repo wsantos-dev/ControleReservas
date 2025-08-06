46d3425fbec0d447ac67e769795bbb0ad00ddd33
Wellington Santos <dev.wsantos@outlook.com>
2025-08-06 13:47:33 -0300

    feat: Adicionada interface do repository genérico

---
+++ src/ControleReservas.Domain/Interfaces/IRespository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq.Expressions;
+
+namespace ControleReservas.Domain.Interfaces;
+
+public interface IRespository<T> where T : class
+{
+    Task<T> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+
+}
