c1849c92a2d5f55dcb53e188fa28c19499d089a9
Wellington Santos <dev.wsantos@outlook.com>
2025-08-07 14:08:43 -0300

    chore: migrations AlteracaoDePropriedadesEntidadeReserva.cs

---
+++ src/ControleReservas.Infrastructure/Migrations/20250807170104_AlteracaoDeProriedadesEntidadeReserva.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleReservas.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteracaoDeProriedadesEntidadeReserva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataHoraReserva",
+                table: "Reservas",
+                newName: "DataHoraInicio");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCancelamento",
+                table: "Reservas",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataHoraFim",
+                table: "Reservas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCancelamento",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "DataHoraFim",
+                table: "Reservas");
+
+            migrationBuilder.RenameColumn(
+                name: "DataHoraInicio",
+                table: "Reservas",
+                newName: "DataHoraReserva");
+        }
+    }
+}
